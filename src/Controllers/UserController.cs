a9d891c0642df40423e21c355f16de325afdf3da
MigDwinta <98965722+MigDwinta@users.noreply.github.com>
2022-02-22 22:09:47 +0700

    add register function

--- src/Controllers/UserController.cs
+++ src/Controllers/UserController.cs
@@ -73,11 +73,55 @@ namespace src.Controllers
             if(logs.Count != 1) {
                 return false;
             }
-            Console.WriteLine("sampe sini");
 
             logs[0].Status = false;
             await _context.SaveChangesAsync();
             return true;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> register([FromBody] RegisterDto input)
+        {
+            if(input.Username.Length < 3 || input.Password.Length < 3 || input.Email == "" || input.Name == "") 
+                return BadRequest();
+
+            var users = await _context.User.Where(x => x.Username == input.Username).ToListAsync();
+
+            if (users.Count != 0)
+                return BadRequest();
+
+            string password = input.Password;
+
+            // generate a 128-bit salt using a cryptographically strong random sequence of nonzero values
+            byte[] salt = new byte[128 / 8];
+            using (var rngCsp = new RNGCryptoServiceProvider())
+            {
+                rngCsp.GetNonZeroBytes(salt);
+            }
+
+            // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8));
+
+            var insert = new User { 
+                Name = input.Name,
+                Username = input.Username,
+                Email = input.Email,
+                PasswordSalt = Convert.ToBase64String(salt),
+                Password = hashed
+            };
+
+            await _context.User.AddAsync(insert);
+            await _context.SaveChangesAsync();
+
+            string token = await _tm.CreateToken(insert);
+
+            Response.Headers.Add("Authorization", token);
+            return insert;
         }
     }
 }
---
+++ src/Dtos/user/RegisterDto.cs
@@ -0,0 +1,10 @@
+namespace src.Dtos
+{
+    public class RegisterDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
