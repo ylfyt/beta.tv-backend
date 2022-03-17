d6fda69671ccffdee87d3ad9438008990afabde6
yudialfayat <ayudi3521@gmail.com>
2022-03-17 19:58:44 +0700

    adding primary key for history

--- src/Models/History.cs
+++ src/Models/History.cs
@@ -2,6 +2,7 @@
 {
     public class History
     {
+        public int Id { get; set; }
         public int UserId { get; set; }
         public int VideoId { get; set; }
         public DateTime Access_Video { get; set; }
