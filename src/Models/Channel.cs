69507c7ecb8bd6ae3aa4a789a2633491cb2fd43d
Vincent Hasiholan <13518108@std.stei.itb.ac.id>
2022-03-17 12:32:58 +0000

    Upload New File

---
+++ src/Models/Channel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace if3250_2022_01_buletin_backend.src.Models
+{
+    public class Channel
+    {
+        public int ID_Channel { get; set; }
+        public string Channel_Owner { get; set; }
+        public int Subscriber_Count { get; set; }
+    }
+}
