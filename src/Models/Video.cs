4fa5ae7e2a7402d2b647a646d4ca5c654586efa5
Vincent Hasiholan <13518108@std.stei.itb.ac.id>
2022-03-17 12:32:46 +0000

    Upload New File

---
+++ src/Models/Video.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace if3250_2022_01_buletin_backend.src.Models
+{
+    public class Video
+    {
+        public int ID_Video { get; set; }
+        public string Video_Creator { get; set; }
+        public string Link_Video { get; set; }
+        public int Views { get; set; }
+        public float Rating { get; set; }
+        public string Kategori { get; set; }
+        public DateTime Release_Date { get; set; }
+    }
+}
