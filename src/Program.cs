496c1f77f31078a964aed1ed50e10bfd7faffc90
yudialfayat <ayudi3521@gmail.com>
2022-03-18 07:45:47 +0700

    AllowCorsOrigin

--- src/Program.cs
+++ src/Program.cs
@@ -34,6 +34,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -43,6 +48,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("corsapp");
 app.UseAuthorization();
 
 app.MapControllers();
