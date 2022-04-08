7a9ac2d95c7c5cda2a8d7f7617a1c285921c9f73
Neptune3X <52845093+Neptune3X@users.noreply.github.com>
2022-04-08 11:09:24 +0700

    Add Model-Videos

--- src/Models/Video.cs
+++ src/Models/Video.cs
@@ -1,22 +1,34 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace src.Models
 {
     public class Video
     {
+        [Key]
         public int Id { get; set; }
         public string YoutubeVideoId { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(50, ErrorMessage = "Title of the Video cannot be more than 50 characters!")]
         public string Title { get; set; } = string.Empty;
         public string ThumbnailUrl { get; set; } = string.Empty;
         public string ChannelThumbnailUrl { get; set; } = string.Empty;
+        [ForeignKey("Channel")]
+        [Column(Order=1)]
         public string ChannelId { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(50, ErrorMessage = "Channel Name cannot be more than 50 characters!")]
         public string ChannelName { get; set; } = string.Empty;
         public string Url { get; set; } = string.Empty;
         public List<string> Categories { get; set; } = new List<string>();
         public string Description { get; set; } = string.Empty;
         public string CreateAt { get; set; } = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+        [Required]
+        [MaxLength(1000, ErrorMessage = "Description of the Author cannot be more than 50 characters!")]
         public string AuthorDescription { get; set; } = string.Empty;
         public string AuthorTitle { get; set; } = string.Empty;
+        [ForeignKey("User")]
+        [Column(Order=2)]
         public int AuthorId { get; set; }
         public string AuthorName { get; set; } = string.Empty;
     }
