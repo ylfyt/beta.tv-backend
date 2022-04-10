78b5764f6122fd6f27715d04fa317b32d45632c5
Neptune3X <52845093+Neptune3X@users.noreply.github.com>
2022-04-10 12:19:27 +0700

    Minor change in Bookmark.cs

--- src/Models/Bookmark.cs
+++ src/Models/Bookmark.cs
@@ -7,7 +7,7 @@ namespace src.Models
 {
     public class Bookmark
     {
-        public int Id_Bookmark { get; set; }
+        public int Id { get; set; }
         public int ChannelId { get; set; }
         public int Id_Video { get; set; }
         public int Id_User { get; set; }
