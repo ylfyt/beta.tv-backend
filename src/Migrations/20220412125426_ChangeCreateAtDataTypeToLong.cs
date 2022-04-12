63d88c613c853cb2f586e017a5f8fb9fb76a9b98
yudialfayat <ayudi3521@gmail.com>
2022-04-12 20:05:03 +0700

    Migration Chanre CreateAt column type in videos

---
+++ src/Migrations/20220412125426_ChangeCreateAtDataTypeToLong.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace if3250_2022_01_buletin_backend.src.Migrations
+{
+    public partial class ChangeCreateAtDataTypeToLong : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "CreateAt",
+                table: "Videos",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CreateAt",
+                table: "Videos",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
